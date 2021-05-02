03137af842a09c560a53b37df9366a7bd25b4e5b
Lugana707 <sam.m.albon@outlook.com>
2021-05-02 19:18:59 -0400

    Added filter for recipes that include all ingredients

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,12 +1,27 @@
 module Api
   class RecipesController < ApplicationController
     def recipes_index
-      @recipes = Recipe.all
+      @recipes = ::Recipe.all
+
+      if params[:ingredients] && params[:ingredients].length > 0
+        ingredientIds = params[:ingredients].split(',')
+
+        recipeIds = ::Ingredient
+          .where(id: ingredientIds)
+          .joins("INNER JOIN ingredients_recipes ON ingredients_recipes.ingredient_id = ingredients.id")
+          .group("ingredients_recipes.recipe_id")
+          .having("COUNT(ingredients_recipes.recipe_id) >= ?", ingredientIds.length)
+          .pluck("ingredients_recipes.recipe_id")
+
+        @recipes = @recipes.where(id: recipeIds)
+      end
+
       render json: @recipes
     end
 
     def ingredients_index
-      @ingredients = Ingredient.all
+      @ingredients = ::Ingredient.all
+
       render json: @ingredients
     end
   end
